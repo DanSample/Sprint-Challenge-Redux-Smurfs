3ec49579aaff306684a1be2add5e6b25461a5c67
Dan Sample <danssample@gmail.com>
2019-06-29 13:03:15 -0700

    Built out first action creator

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -3,6 +3,14 @@
   Be sure to export each action type so you can pull it into your reducer
 */
 
+import axios from 'axios';
+
+export const FETCHING_DATA = 'FETCHING_DATA';
+export const FETCHING_SUCCESS = 'FETCHING_SUCCESS';
+export const FETCHING_FAILURE = 'FETCHING_FAILURE';
+
+const URL = 'http://localhost:3333/smurfs';
+
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
@@ -13,3 +21,12 @@
    U - updateSmurf
    D - deleteSmurf
 */
+export const fetchSmurfs = () => async dispatch => {
+  dispatch({ type: FETCHING_DATA });
+  try {
+    const { data } = await axios.get(`${URL}`);
+    dispatch({ type: FETCHING_SUCCESS, payload: data });
+  } catch (err) {
+    dispatch({ type: FETCHING_FAILURE, payload: err });
+  }
+};
